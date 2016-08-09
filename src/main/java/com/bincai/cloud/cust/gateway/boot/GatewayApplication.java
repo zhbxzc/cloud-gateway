c9e57213b086fe4106bef9ec634a3d1a7df1aec6
zhanghb22 <329458839@qq.com>
2016-08-09 08:47:10 +0800

    commit

---
+++ src/main/java/com/bincai/cloud/cust/gateway/boot/GatewayApplication.java
@@ -0,0 +1,33 @@
+package com.bincai.cloud.cust.gateway.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+/*import org.springframework.cloud.sleuth.Sampler;*/
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication
+{
+	/*@Bean
+	Sampler sampler()
+	{
+		return span -> true;
+	}*/
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	/**
+	 * 执行请求前的过滤器
+	 * @return
+	 */
+	@Bean
+	public PreFilter simpleFilter()
+	{
+		return new PreFilter();
+	}
+}
